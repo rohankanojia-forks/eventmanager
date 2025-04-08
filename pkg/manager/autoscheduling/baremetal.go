5c7206c8f2e5ea87956c4cee0cfc557c0473bf17
Rohan Kumar <rohaan@redhat.com>
2025-04-08 21:54:56 +0530

    fix : update baremetal logic to read from Secret mounted as volumes instead of environment variables
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

--- pkg/manager/autoscheduling/baremetal.go
+++ pkg/manager/autoscheduling/baremetal.go
@@ -1,20 +1,25 @@
 package autoscheduling
 
 import (
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"github.com/devtools-qe-incubator/eventmanager/pkg/util/logging"
 	"golang.org/x/crypto/ssh"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
 
+var (
+	BareMetalMachineSecretMountPath = "/etc/%s"
+)
+
 type BareMetalMachineConfig struct {
-	Username       string `json:"username"`
-	Host           string `json:"host"`
-	PublicKey      string `json:"id_rsa"`
+	Username  string `json:"username"`
+	Host      string `json:"host"`
+	PublicKey string `json:"id_rsa"`
+	// TODO: Should we also specify what command to run to find whether machine is free
+	// and expected output?
 	Command        string `json:"command"`
 	ExpectedResult string `json:"expectedResult"`
 }
@@ -36,28 +41,69 @@ func InspectFreeBareMetalMachines() []string {
 }
 
 func isBareMetalMachineAvailable(machine string) bool {
-	bareMetalMachineConfigStr := os.Getenv(machine)
-	if bareMetalMachineConfigStr == "" {
+	config, err := getBareMetalMachineConfigFromMountedSecret(machine)
+	if err != nil {
+		logging.Errorf("Error getting bare metal machine config from mounted secret for %s: %v", machine, err)
 		return false
 	}
 
-	var config BareMetalMachineConfig
-	err := json.Unmarshal([]byte(bareMetalMachineConfigStr), &config)
-	if err != nil {
-		logging.Errorf("error unmarshaling BareMetalMachineConfig: %v", err)
-		return false
-	}
 	return createSshConnectionAndExecuteCommand(config)
 }
 
-func createSshConnectionAndExecuteCommand(config BareMetalMachineConfig) bool {
-	key, err := base64.StdEncoding.DecodeString(config.PublicKey)
+func getBareMetalMachineConfigFromMountedSecret(machine string) (*BareMetalMachineConfig, error) {
+	machineSecretDir := fmt.Sprintf(BareMetalMachineSecretMountPath, machine)
+	if _, err := os.Stat(machineSecretDir); os.IsNotExist(err) {
+		return nil, fmt.Errorf("baremetal machine secret directory does not exist: %s", machineSecretDir)
+	} else if err != nil {
+		return nil, fmt.Errorf("error checking baremetal machine secret directory: %w", err)
+	}
+	logging.Info("Found baremetal machine secret directory")
+	config := &BareMetalMachineConfig{}
+
+	dir, err := os.Open(machineSecretDir)
 	if err != nil {
-		logging.Info("Failed to read private key:", err)
-		return false
+		return nil, fmt.Errorf("error opening secret directory: %w", err)
+	}
+	defer dir.Close()
+
+	fileNames, err := dir.Readdirnames(-1) // -1 reads all entries
+	if err != nil {
+		return nil, fmt.Errorf("error reading file names from baremetal machine secret directory: %w", err)
 	}
 
-	signer, err := ssh.ParsePrivateKey(key)
+	for _, filename := range fileNames {
+		filePath := filepath.Join(machineSecretDir, filename)
+		info, _ := os.Stat(filePath)
+		if !info.IsDir() {
+			contentBytes, err := os.ReadFile(filePath)
+			if err != nil {
+				return nil, fmt.Errorf("error reading file %s: %w", filePath, err)
+			}
+			content := strings.TrimSuffix(string(contentBytes), "\n")
+
+			switch filename {
+			case "user":
+				config.Username = content
+			case "host":
+				config.Host = content
+			case "key":
+				config.PublicKey = content
+				// TODO: Shall we also specify what command to run to find a specific instance is available or not?
+			case "command":
+				config.Command = content
+				// TODO: If we specify what command to run, we should also specify expected output to consider a machine available.
+			case "expectedResult":
+				config.ExpectedResult = content
+			}
+		}
+	}
+
+	logging.Info("Baremetal machine secret directory found, created obj ", config)
+	return config, nil
+}
+
+func createSshConnectionAndExecuteCommand(config *BareMetalMachineConfig) bool {
+	signer, err := ssh.ParsePrivateKey([]byte(config.PublicKey))
 	if err != nil {
 		logging.Info("Failed to parse private key:", err)
 		return false
